2ad630b83ec2e5be84ac3f57e75be13070e43341
Jeffrey <jlee2978@uwo.ca>
2021-03-01 01:38:28 -0500

    Added upload functionality

--- GUI/index.js
+++ GUI/index.js
@@ -1,7 +1,25 @@
 function uploadData() {
-    // non-functional stub
-    
-    // if no file chosen to upload, send notification alert
-    // else:
-    alert("Stub: Backend database needs to accept uploaded file!");
+
+	var fileUpload = document.getElementById('uploadedFile');
+
+	const files = fileUpload.files
+
+	const formData = new FormData()
+	formData.append('myFile', files[0])
+
+
+	fetch('http://localhost:8080/upload', {
+		method: 'POST',
+		body: formData
+	})
+		.then(response => response.json())
+		.then(data => {
+			alert('File is uploaded successfully and is saved under ' + data.path)
+
+			console.log(data.path)
+		})
+		.catch(error => {
+			alert('Error ' + error)
+			console.error(error)
+		})
 }
